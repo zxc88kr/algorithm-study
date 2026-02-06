2fba75fbafbc6d764f61cab10d91056c26cfbd32
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-06 19:57:30 +0900

    [Bronze V] Title: CAPS, Time: 4 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/15000. CAPS/CAPS.cc
+++ 백준/Bronze/15000. CAPS/CAPS.cc
@@ -3,12 +3,12 @@
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
+    std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     
     std::string str;
     std::cin >> str;
-    for (int i = 0; i < str.length(); i++)
-        str[i] = std::toupper(str[i]);
+    for (char& c : str)
+        c = std::toupper(c);
     std::cout << str;
 }

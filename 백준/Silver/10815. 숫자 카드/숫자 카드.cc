9e480680322b5d742dfdcf5302d93258688ac628
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-13 20:21:42 +0900

    [Silver V] Title: 숫자 카드, Time: 164 ms, Memory: 5892 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -6,23 +6,26 @@ int main()
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
-    std::cout.tie(NULL);
-
-    int N, M, temp;
-    std::cin >> N;
-    std::vector<int> have(N);
-    for (int i = 0; i < N; i++)
+    
+    int n;
+    std::cin >> n;
+    
+    std::vector<int> havings(n);
+    for (int i = 0; i < n; i++)
+        std::cin >> havings[i];
+    std::sort(havings.begin(), havings.end());
+    
+    int m;
+    std::cin >> m;
+    
+    std::vector<int> cards(m);
+    for (int i = 0; i < m; i++)
+        std::cin >> cards[i];
+    
+    for (auto card : cards)
     {
-        std::cin >> temp;
-        have[i] = temp;
-    }
-    std::sort(have.begin(), have.end());
-
-    std::cin >> M;
-    for (int i = 0; i < M; i++)
-    {
-        std::cin >> temp;
-        if (std::binary_search(have.begin(), have.end(), temp)) std::cout << "1 ";
-        else std::cout << "0 ";
+        if (std::binary_search(havings.begin(), havings.end(), card))
+            std::cout << 1 << ' ';
+        else std::cout << 0 << ' ';
     }
 }

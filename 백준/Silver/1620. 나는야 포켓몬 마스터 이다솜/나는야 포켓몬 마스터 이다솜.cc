e39f8492137fe90579955dfe6bdc8e913fe21374
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-17 05:14:38 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 80 ms, Memory: 20664 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,33 +1,32 @@
 #include <iostream>
-#include <string>
 #include <unordered_map>
+#include <string>
+#include <vector>
 
 int main()
 {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(NULL);
-	std::cout.tie(NULL);
-
-	int N, M;
-	std::cin >> N >> M;
-
-	std::unordered_map<std::string, int> pocket_to_num;
-	std::string num_to_pocket[N + 1];
-
-	for (int i = 0; i < N; i++)
-	{
-		std::string str;
-		std::cin >> str;
-		pocket_to_num[str] = i + 1;
-		num_to_pocket[i + 1] = str;
-	}
-	for (int i = 0; i < M; i++)
-	{
-		std::string str;
-		std::cin >> str;
-		if (str[0] >= 'A' && str[0] <= 'Z')
-			std::cout << pocket_to_num[str] << '\n';
-		else if (str[0] >= '1' && str[0] <= '9')
-			std::cout << num_to_pocket[std::stoi(str)] << '\n';
-	}
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    int n, m;
+    std::cin >> n >> m;
+    
+    std::unordered_map<std::string, int> pocket_to_num;
+    std::vector<std::string> num_to_pocket(n + 1);
+    
+    for (int i = 1; i <= n; i++)
+    {
+        std::cin >> num_to_pocket[i];
+        pocket_to_num[num_to_pocket[i]] = i;
+    }
+    
+    std::string str;
+    for (int i = 0; i < m; i++)
+    {
+        std::cin >> str;
+        if (std::isdigit(str[0]))
+            std::cout << num_to_pocket[std::stoi(str)] << '\n';
+        else
+            std::cout << pocket_to_num[str] << '\n';
+    }
 }

4f79d4d72b64b99e102eb1947ebdffe3aa667769
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-17 18:09:26 +0900

    [Silver IV] Title: 다음 소수, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.cc
+++ 백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -1,30 +1,33 @@
-#include <cstdio>
+#include <iostream>
 
-bool isPrime(long num) {
+bool isPrime(long num)
+{
     if (num == 1) return false;
     if (num == 2) return true;
     if (num % 2 == 0) return false;
     
-    for (long i = 3; i * i <= num; i += 2) {
-        if (num % i == 0) {
+    for (long i = 3; i * i <= num; i += 2)
+        if (num % i == 0)
             return false;
-        }
-    }
     return true;
 }
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; i++)
+    {
         long num;
-        scanf("%ld", &num);
-        while (true) {
-            if (isPrime(num)) {
-                printf("%ld\n", num);
-                break;
-            }
+        std::cin >> num;
+        while (true)
+        {
+            if(isPrime(num)) break;
             num++;
         }
+        std::cout << num << '\n';
     }
 }

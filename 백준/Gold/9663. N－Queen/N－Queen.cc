794c63821135fe1b56d1941a4cfc342e198df8a5
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-24 06:02:42 +0900

    [Gold IV] Title: N-Queen, Time: 1812 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+bool visited[15];
+bool check1[30];
+bool check2[30];
+
+int count;
+
+void dfs(int row, int n)
+{
+    if (row == n)
+    {
+        count++;
+        return;
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i] || check1[row + i] || check2[row - i + n]) continue;
+        visited[i] = check1[row + i] = check2[row - i + n] = true;
+        dfs(row + 1, n);
+        visited[i] = check1[row + i] = check2[row - i + n] = false;
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    int n;
+    std::cin >> n;
+    
+    dfs(0, n);
+    std::cout << count;
+}

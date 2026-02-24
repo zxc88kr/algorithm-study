7cb1a7df87559ce0bd0dc4f67f5bcb585c0b77a1
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-24 17:12:48 +0900

    [Gold IV] Title: 스도쿠, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+std::vector<std::pair<int, int>> blank;
+
+int sudoku[9][9];
+bool box[9][10];
+bool row[9][10];
+bool col[9][10];
+
+bool flag = false;
+
+void dfs(int solve)
+{
+    if (flag) return;
+    if (solve == blank.size())
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+                std::cout << sudoku[i][j] << ' ';
+            std::cout << '\n';
+        }
+        flag = true;
+        return;
+    }
+    
+    int i = blank[solve].first;
+    int j = blank[solve].second;
+    
+    for (int x = 1; x <= 9; x++)
+    {
+        if (box[(i/3) * 3 + (j/3)][x] || row[i][x] || col[j][x]) continue;
+        box[(i/3) * 3 + (j/3)][x] = row[i][x] = col[j][x] = true;
+        sudoku[i][j] = x;
+        dfs(solve + 1);
+        sudoku[i][j] = 0;
+        box[(i/3) * 3 + (j/3)][x] = row[i][x] = col[j][x] = false;
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    
+    int x;
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+        {
+            std::cin >> x;
+            if (x == 0)
+            {
+                blank.push_back({i, j});
+                continue;
+            }
+            sudoku[i][j] = x;
+            box[(i/3) * 3 + (j/3)][x] = row[i][x] = col[j][x] = true;
+        }
+    
+    dfs(0);
+}

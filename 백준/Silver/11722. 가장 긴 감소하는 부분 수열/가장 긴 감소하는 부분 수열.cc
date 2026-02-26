f785dcc61f66b7d337e9896b13c19eadac0e0271
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-26 21:04:23 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    
+    std::vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        std::cin >> arr[i];
+    
+    std::vector<int> dp(n, 1);
+    for (int i = 1; i < n; i++)
+        for (int j = 0; j < i; j++)
+            if (arr[i] < arr[j])
+                dp[i] = std::max(dp[i], dp[j] + 1);
+    
+    std::cout << *std::max_element(dp.begin(), dp.end());
+}

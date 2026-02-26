615ab9e02d4e63140f0f664abb3f5909f9da7b28
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-26 21:58:54 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 144 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+        
+    int n;
+    std::cin >> n;
+
+    std::vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        std::cin >> arr[i];
+    
+    std::vector<int> lis;
+    for (int i = 0; i < n; i++)
+    {
+        auto it = std::lower_bound(lis.begin(), lis.end(), arr[i]);
+        if (it == lis.end()) lis.push_back(arr[i]);
+        else *it = arr[i];
+    }
+    std::cout << lis.size();
+}

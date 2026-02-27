49305de602098e231dab706b1840a793edd7c8ab
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-28 00:35:05 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    int n;
+    std::cin >> n;
+    
+    int x;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> x;
+        if (x == 0)
+        {
+            if (!pq.empty())
+            {
+                std::cout << pq.top() << '\n';
+                pq.pop();
+            }
+            else std::cout << 0 << '\n';
+        }
+        else pq.push(x);
+    }
+}

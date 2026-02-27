0999fbbb39e782217494d846573af12fb28dd627
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-27 17:39:46 +0900

    [Gold IV] Title: 체스판 다시 칠하기 2, Time: 68 ms, Memory: 33480 KB -BaekjoonHub

--- 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.cc
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <cmath>
 
 int main()
@@ -13,22 +14,22 @@ int main()
     std::vector<std::vector<int>> b_board(n + 1, std::vector<int>(m + 1, 0));
     std::vector<std::vector<int>> w_board(n + 1, std::vector<int>(m + 1, 0));
     for (int i = 1; i <= n; i++)
+    {
+        std::string str;
+        std::cin >> str;
         for (int j = 1; j <= m; j++)
         {
             char target;
             if ((i + j) % 2 == 0) target = 'B';
             else target = 'W';
             
-            char ch;
-            std::cin >> ch;
-            
-            if (ch == target) b_board[i][j] = 1;
-            else b_board[i][j] = 0;
-            w_board[i][j] = b_board[i][j] ^ 1;
+            if (str[j - 1] == target) b_board[i][j]++;
+            else w_board[i][j]++;
             
             b_board[i][j] += b_board[i][j - 1] + b_board[i - 1][j] - b_board[i - 1][j - 1];
             w_board[i][j] += w_board[i][j - 1] + w_board[i - 1][j] - w_board[i - 1][j - 1];
         }
+    }
     
     int min_change = n * m;
     for (int i = k; i <= n; i++)

4f7560ab4849aa458c233cf02cf8dedce396e1d5
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-02-28 01:00:12 +0900

    [Silver I] Title: 절댓값 힙, Time: 12 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
+
+struct Compare {
+  bool operator() (const int& a, const int& b)
+  {
+      if (std::abs(a) == std::abs(b))
+          return a > b;
+      return std::abs(a) > std::abs(b);
+  }
+};
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    int n;
+    std::cin >> n;
+    
+    int x;
+    std::priority_queue<int, std::vector<int>, Compare> pq;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> x;
+        if (x == 0)
+        {
+            if (!pq.empty())
+            {
+                std::cout << pq.top() << '\n';
+                pq.pop();
+            }
+            else std::cout << 0 << '\n';
+        }
+        else pq.push(x);
+    }
+}

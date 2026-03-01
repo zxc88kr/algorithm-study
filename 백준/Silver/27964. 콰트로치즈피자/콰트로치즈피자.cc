1658019ab095bb6852050b0a1243f9e948b0657d
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-02 08:15:13 +0900

    [Silver V] Title: 콰트로치즈피자, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/27964. 콰트로치즈피자/콰트로치즈피자.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_set>
+#include <string>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    
+    std::unordered_set<std::string> st;
+    std::string str;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> str;
+        st.insert(str);
+    }
+    
+    int count = 0;
+    for (auto topping : st)
+    {
+        if (topping.size() < 6) continue;
+        
+        if (topping.substr(topping.size() - 6, 6) == "Cheese") count++;
+        if (count == 4) break;
+    }
+    std::cout << ((count == 4) ? "yummy" : "sad"); 
+}

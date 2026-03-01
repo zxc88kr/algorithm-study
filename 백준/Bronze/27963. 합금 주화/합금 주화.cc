83049a1973334dfcc56ac53babd9a62263ee39c7
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-02 07:54:23 +0900

    [Bronze I] Title: 합금 주화, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27963. 합금 주화/합금 주화.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    int d1, d2, x;
+    std::cin >> d1 >> d2 >> x;
+    
+    if (d2 > d1) std::swap(d1, d2);
+
+    std::cout << std::fixed;
+    std::cout << (double)(d1 * d2 * 100) / (x * d2 + (100 - x) * d1);
+}

2e14255a36df8360b43c0f696d1dbfb6408d1c26
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-03 12:00:58 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 76 ms, Memory: 10928 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::vector<int>> graph;
+std::vector<int> visited_order;
+
+int order = 1;
+
+void dfs(int node)
+{
+    visited_order[node] = order++;
+    for (int next : graph[node])
+        if (visited_order[next] == 0) dfs(next);
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    int n, m, r;
+    std::cin >> n >> m >> r;
+    
+    graph.resize(n + 1);
+    visited_order.resize(n + 1, 0);
+    
+    int u, v;
+    for (int i = 0; i < m; i++)
+    {
+        std::cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    
+    for (int i = 1; i <= n; i++)
+        std::sort(graph[i].begin(), graph[i].end(), std::greater<int>());
+    
+    dfs(r);
+    
+    for (int i = 1; i <= n; i++)
+        std::cout << visited_order[i] << '\n';
+}

b92dc5d49bd182797e5e3af4bd66c65e5122fcd1
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-03 15:00:08 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 92 ms, Memory: 8840 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+std::vector<std::vector<int>> graph;
+std::vector<int> visited_order;
+
+int order = 1;
+
+void bfs(int start)
+{
+    std::queue<int> q;
+    
+    visited_order[start] = order++;
+    q.push(start);
+    
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+        
+        for (int next : graph[cur])
+            if (visited_order[next] == 0)
+            {
+                visited_order[next] = order++;
+                q.push(next);
+            }
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    int n, m, r;
+    std::cin >> n >> m >> r;
+    
+    graph.resize(n + 1);
+    visited_order.resize(n + 1, 0);
+    
+    int u, v;
+    for (int i = 0; i < m; i++)
+    {
+        std::cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    
+    for (int i = 1; i <= n; i++)
+        std::sort(graph[i].begin(), graph[i].end(), std::greater<int>());
+        
+    bfs(r);
+    
+    for (int i = 1; i <= n; i++)
+        std::cout << visited_order[i] << '\n';
+}

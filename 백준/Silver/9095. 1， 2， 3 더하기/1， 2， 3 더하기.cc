4f18dcde711121ddaa7e6f327a56a7a93ca8ba44
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-05 07:14:41 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    int dp[12];
+    
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    
+    for (int i = 4; i <= 11; i++)
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    
+    int t;
+    std::cin >> t;
+    
+    while (t--)
+    {
+        int n;
+        std::cin >> n;
+        
+        std::cout << dp[n] << '\n';
+    }
+}

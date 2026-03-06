a23c541132c42564c4a89c9267a4d4fb6caa43a0
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-06 23:39:11 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 100 ms, Memory: 12752 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> graph;
+std::vector<int> visited, result;
+
+void dfs(int cur)
+{
+    for (int next : graph[cur])
+        if (!visited[next])
+        {
+            visited[next] = true;
+            result[next] = cur;
+            dfs(next);
+        }
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    
+    graph.resize(n + 1);
+    visited.resize(n + 1);
+    result.resize(n + 1);
+    
+    int a, b;
+    for (int i = 0; i < n - 1; i++)
+    {
+        std::cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    
+    visited[1] = true;
+    dfs(1);
+    
+    for (int i = 2; i <= n; i++)
+        std::cout << result[i] << '\n';
+}

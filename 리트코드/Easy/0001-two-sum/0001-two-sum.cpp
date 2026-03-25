91c0ac35014e090f8d4557a158cda7d7adcffa62
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-25 18:11:46 +0000

    Revert "[Easy] Title: 0001-two-sum, Time: N/A, Memory: N/A -LeetHub"
    This reverts commit 62e9f06a5ee88c2cda92ad2fed52ac623cacac75.

--- 리트코드/Easy/0001-two-sum/0001-two-sum.cpp
+++ 리트코드/Easy/0001-two-sum/0001-two-sum.cpp
@@ -5,13 +5,14 @@ class Solution
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target)
     {
-        
+        std::vector<int> result(2);
         for (int i = 0; i < nums.size() - 1; i++)
             for (int j = i + 1; j < nums.size(); j++)
                 if (nums[i] + nums[j] == target)
                 {
-                    std::vector<int> result = { i, j };
-                    return result;
+                    result[0] = i;
+                    result[1] = j;
                 }
+        return result;
     }
 };

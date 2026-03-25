3e0c9ce360ec8b5de7b68e68bac32b6c5775cca4
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-26 02:50:28 +0900

    [Easy] Title: 0001-two-sum, Time: 69 ms, Memory: 13.9 MB -LeetHub

---
+++ 리트코드/Easy/0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+class Solution
+{
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target)
+    {
+        std::vector<int> result(2);
+        for (int i = 0; i < nums.size() - 1; i++)
+            for (int j = i + 1; j < nums.size(); j++)
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                }
+        return result;
+    }
+};

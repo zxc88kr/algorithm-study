3deb1d08af0648fb18b822bec9f6db23f778dbfa
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-26 04:50:14 +0900

    [Easy] Title: 0001-two-sum, Time: N/A, Memory: N/A -LeetHub

--- 리트코드/Easy/0001-two-sum/0001-two-sum.cpp
+++ 리트코드/Easy/0001-two-sum/0001-two-sum.cpp
@@ -5,7 +5,7 @@ class Solution
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target)
     {
-        std::vector<int> result;
+        std::vector<int> result(2);
         for (int i = 0; i < nums.size() - 1; i++)
             for (int j = i + 1; j < nums.size(); j++)
                 if (nums[i] + nums[j] == target)

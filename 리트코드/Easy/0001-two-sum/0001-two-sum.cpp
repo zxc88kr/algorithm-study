5577e18209ac08a0358546efabcda633831fc65d
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-26 04:50:38 +0900

    [Easy] Title: 0001-two-sum, Time: 63 ms, Memory: 14 MB -LeetHub

--- 리트코드/Easy/0001-two-sum/0001-two-sum.cpp
+++ 리트코드/Easy/0001-two-sum/0001-two-sum.cpp
@@ -9,7 +9,10 @@ public:
         for (int i = 0; i < nums.size() - 1; i++)
             for (int j = i + 1; j < nums.size(); j++)
                 if (nums[i] + nums[j] == target)
-                    result = { i, j };
+                {
+                    result[0] = i;
+                    result[1] = j;
+                }
         return result;
     }
 };

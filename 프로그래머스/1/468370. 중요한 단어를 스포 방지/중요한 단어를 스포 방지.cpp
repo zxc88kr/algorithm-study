919972de2d20edaf91d7bee5f2f0a745d265c824
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-25 21:15:54 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 0.50 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.cpp
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.cpp
@@ -1,35 +1,40 @@
 #include <string>
 #include <sstream>
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
 
 int solution(std::string message, std::vector<std::vector<int>> spoiler_ranges)
 {
-    std::string blind_message = message;
+    int answer = 0;
     
-    for (auto range : spoiler_ranges)
-        for (int idx = range[0]; idx <= range[1]; idx++)
-            if (blind_message[idx] != ' ')
-                blind_message[idx] = '*';
+    std::unordered_map<std::string, int> m;
     
-    std::unordered_set<std::string> st;
-    
-    std::stringstream ss(blind_message);
+    std::stringstream ss(message);
     std::string word;
     
-    while (std::getline(ss, word, ' '))
-        st.insert(word);
+    while (std::getline(ss, word, ' ')) m[word]++;
     
-    ss.clear();
-    ss.str(message);
-
-    int answer = 0;
-    while (std::getline(ss, word, ' '))
-        if (!st.count(word))
-        {
-            st.insert(word);
-            answer++;
-        }
+    int prev_end = 0;
+    for (auto range : spoiler_ranges)
+    {
+        int start = std::max(range[0], prev_end);
+        int end = range[1];
+        
+        if (start > end) continue;
+        
+        while (start >= 0 && message[start] != ' ') start--;
+        while (end < message.size() && message[end] != ' ') end++;
+        
+        start++;
+        
+        std::stringstream ss(message.substr(start, end - start));
+        while (std::getline(ss, word, ' ')) m[word]--;
+        
+        prev_end = end + 1;
+    }
+    
+    for (auto it = m.begin(); it != m.end(); it++)
+        if (it->second == 0) answer++;
     
     return answer;
 }

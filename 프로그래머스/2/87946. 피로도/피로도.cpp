e9b83d20fa90bb3663013bc8bb5fd3a218305b7b
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-26 22:59:20 +0900

    [level 2] Title: 피로도, Time: 1.03 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+std::vector<bool> visited;
+
+int max = -1;
+
+void dfs(int depth, int tire, const std::vector<std::vector<int>>& dungeons)
+{
+    if (depth > max) max = depth;
+    
+    for (int i = 0; i < dungeons.size(); i++)
+        if (!visited[i] && tire >= dungeons[i][0])
+        {
+            visited[i] = true;
+            tire -= dungeons[i][1];
+            dfs(depth + 1, tire, dungeons);
+            tire += dungeons[i][1];
+            visited[i] = false;
+        }
+}
+
+int solution(int k, std::vector<std::vector<int>> dungeons)
+{
+    visited.assign(dungeons.size(), false);
+    
+    dfs(0, k, dungeons);
+    
+    return max;
+}

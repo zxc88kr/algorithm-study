910d0d8f2e0b6a5b76dee27e9b68673260eab595
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-27 03:54:19 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -3,5 +3,5 @@ FROM PLACES
 WHERE HOST_ID IN (SELECT HOST_ID
                   FROM PLACES
                   GROUP BY HOST_ID
-                  HAVING COUNT(DISTINCT NAME) >= 2)
+                  HAVING COUNT(*) >= 2)
 ORDER BY ID

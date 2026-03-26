f4f0b6a2207463254e652f32e8a4bd35350d68be
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-26 22:32:23 +0900

    [level 2] Title: 타겟 넘버, Time: 3.08 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,7 +3,7 @@
 int count = 0;
 int result = 0;
 
-void dfs(int depth, const std::vector<int>& numbers, int target)
+void dfs(int depth, const std::vector<int>& numbers, int result, int target)
 {
     if (depth == numbers.size())
     {
@@ -11,18 +11,13 @@ void dfs(int depth, const std::vector<int>& numbers, int target)
         return;
     }
     
-    result -= numbers[depth];
-    dfs(depth + 1, numbers, target);
-    result += numbers[depth];
-    
-    result += numbers[depth];
-    dfs(depth + 1, numbers, target);
-    result -= numbers[depth];
+    dfs(depth + 1, numbers, result - numbers[depth], target);
+    dfs(depth + 1, numbers, result + numbers[depth], target);
 }
 
 int solution(std::vector<int> numbers, int target)
 {
-    dfs(0, numbers, target);
+    dfs(0, numbers, 0, target);
     
     return count;
 }

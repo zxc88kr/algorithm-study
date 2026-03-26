4bb38f7a42a99120ee15ea11a88c723a8991dcef
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-26 23:43:19 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.42 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream> // 삭제
+
+int solution(std::string s)
+{
+    int len = s.size();
+    s += s;
+    
+    int count = 0;
+    
+    for (int i = 0; i < len; i++)
+    {
+        bool flag = true;
+        std::stack<char> st;
+        
+        for (int j = i; j < i + len; j++)
+        {
+            char ch = s[j];
+            if (ch == '[' || ch == '(' || ch == '{')
+                st.push(ch);
+            else if (st.empty())
+            {
+                flag = false;
+                break;
+            }
+            else if (ch == ']' && st.top() == '[') st.pop();
+            else if (ch == ')' && st.top() == '(') st.pop();
+            else if (ch == '}' && st.top() == '{') st.pop();
+
+        }
+        
+        if (flag && st.empty()) count++;
+    }
+
+    return count;
+}

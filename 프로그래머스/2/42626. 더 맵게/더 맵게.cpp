402feefeab94509e1b42b0f62f4efce8954fba08
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-27 20:34:30 +0900

    [level 2] Title: 더 맵게, Time: 221.13 ms, Memory: 40.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <queue>
+
+int solution(std::vector<int> scoville, int K)
+{
+    std::priority_queue<int, std::vector<int>, std::greater<int>> pq(scoville.begin(), scoville.end());
+    
+    int answer = 0;
+    while (pq.size() > 1 && pq.top() < K)
+    {
+        int low = pq.top();
+        pq.pop();
+        int second_low = pq.top();
+        pq.pop();
+        
+        pq.push(low + second_low * 2);
+        answer++;
+    }
+    
+    if (pq.top() < K) answer = -1;
+
+    return answer;
+}

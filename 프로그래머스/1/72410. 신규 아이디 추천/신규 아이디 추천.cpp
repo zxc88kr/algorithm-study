9029b5a1731f833782afc393d824c5b9d1b1d753
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-03-28 04:27:00 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.40 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <cctype>
 
 std::string solution(std::string new_id)
 {
@@ -6,20 +7,20 @@ std::string solution(std::string new_id)
     
     for (char ch : new_id)
     {
-        if (isalpha(ch)) answer.push_back(std::tolower(ch));
-        else if (isdigit(ch) || ch == '-' || ch == '_' || ch == '.') answer.push_back(ch);
+        if (std::isalpha(ch)) answer.push_back(std::tolower(ch));
+        else if (std::isdigit(ch) || ch == '-' || ch == '_' || ch == '.') answer.push_back(ch);
     }
     
     while (answer.find("..") != std::string::npos)
         answer.replace(answer.find(".."), 2, ".");
     
-    if (answer.front() == '.') answer.erase(0, 1);
-    if (answer.back() == '.') answer.erase(answer.size() - 1, 1);
+    if (!answer.empty() && answer.front() == '.') answer.erase(0, 1);
+    if (!answer.empty() && answer.back() == '.') answer.pop_back();
     
     if (answer.empty()) answer = "a";
     
     if (answer.size() >= 16) answer = answer.substr(0, 15);
-    if (answer.back() == '.') answer.erase(answer.size() - 1, 1);
+    if (answer.back() == '.') answer.pop_back();
     
     if (answer.size() <= 2)
         while (answer.size() < 3)

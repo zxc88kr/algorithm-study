b5183dbb682393fa7bfdcbbd66a811ea85d115be
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-04-02 05:30:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -6,20 +6,13 @@ int main()
     int n;
     std::cin >> n;
     
-    int answer = 0;
+    std::vector<int> dp(n + 1);
     
-    if (n == 1) answer = 1;
-    else if (n == 2) answer = 3;
-    else
-    {
-        std::vector<int> dp(n + 1);
-        dp[1] = 1;
-        dp[2] = 3;
-        
-        for (int i = 3; i <= n; i++)
-            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
-        
-        answer = dp[n];
-    }
-    std::cout << answer;
+    dp[1] = 1;
+    if (n >= 2) dp[2] = 3;
+    
+    for (int i = 3; i <= n; i++)
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+    
+    std::cout << dp[n];
 }

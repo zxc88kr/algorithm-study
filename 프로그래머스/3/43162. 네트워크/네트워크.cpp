cb99a05c5c5792831347c87d55dffbd5b3cc873b
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2026-04-04 01:22:39 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <queue>
+
+std::vector<std::vector<int>> connected;
+std::vector<int> visited;
+
+void bfs(int x)
+{
+    std::queue<int> q;
+    
+    visited[x] = true;
+    q.push(x);
+    
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+        
+        for (int next : connected[cur])
+            if (!visited[next])
+            {
+                visited[next] = true;
+                q.push(next);
+            }
+    }
+}
+
+int solution(int n, std::vector<std::vector<int>> computers)
+{
+    connected.resize(n);
+    visited.assign(n, false);
+    
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j) continue;
+            
+            if (computers[i][j] == 1)
+            {
+                connected[i].push_back(j);
+                connected[j].push_back(i);
+            }
+        }
+    
+    int group_count = 0;
+    for (int i = 0; i < n; i++)
+        if (!visited[i])
+        {
+            bfs(i);
+            group_count++;
+        }
+    
+    return group_count;
+}
